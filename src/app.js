1f3a6c8315b691dbac5ca292a2d996f53fa4c103
palpich <vpkrysin@gmail.com>
2018-05-21 12:48:46 +0300

    Add template.html

--- src/app.js
+++ src/app.js
@@ -1,16 +1,30 @@
 const Vue = require('vue')
-const VueServer = require('vue-server-renderer')
-
-const app = new Vue({
-  template: '<div>Hello World</div>'
+const server = require('express')()
+const renderer = require('vue-server-renderer').createRenderer({
+  template: require('fs').readFileSync(__dirname + '/template.html', 'utf-8')
 })
 
-const renderer = VueServer.createRenderer()
+const context = {
+  title: 'Helllofff'
 
-renderer.renderToString(app)
-  .then(html => {
-    console.log(html)
+}
+server.get('*', (req, res) => {
+  const app = new Vue({
+    data: {
+      url: req.url
+    },
+    template: '<div>Hello World. {{ url }}</div>'
   })
-  .catch(error => {
-    console.log(error)
-  })
+
+  renderer.renderToString(app, context)
+    .then(html => {
+      res.end(html)
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).end('Внутренняя ошибка сервера')
+      return
+    })
+})
+
+server.listen(8080)
