48d71e9a0aaf7685cbe77d8785d02fdd69ba374f
palpich <vpkrysin@gmail.com>
2018-06-10 20:33:34 +0300

    Update entry-server.js

--- src/entry-server.js
+++ src/entry-server.js
@@ -1,6 +1,20 @@
 import { createApp } from './app'
 
 export default context => {
-  const { app } = createApp()
-  return app
+  return new Promise((resolve, reject) => {
+    const { app, router } = createApp()
+
+    // Чтобы сервер знал текущий URL когда будет работать с экземпляром приложения на сервере
+    router.push(context.url)
+
+    router.onReady(() => {
+      const matchedComponents = router.getMatchedComponennts()
+
+      if (!matchedComponents.length) {
+        return reject({ code: 404 })
+      }
+
+      resolve(app)
+    }, reject)
+  })
 }
